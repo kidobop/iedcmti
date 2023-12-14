0ffc56852d8e0394ea9046061cf9038d3dc00491
aaqil <kidobop@gmail.com>
2023-12-14 22:51:23 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '../styles/modern-normalized.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/work.css';
+import '../styles/utlis.css';
