5570934a54d1c7ddc32346e04e6b1b420305bdb6
aaqil <kidobop@gmail.com>
2023-12-14 23:52:22 +0530

    Created a contact section

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/components/work.css';
+import '../styles/components/contact.css';
 import '../styles/utlis.css';
